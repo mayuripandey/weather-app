78186c2e20c6d9c3379714b69b95c89850e121bf
mayuripandey <mayuripandey4@gmail.com>
2021-01-24 22:08:26 +0100

    weather app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import React from 'react';
+const api = {
+  key: "2e9922a52a6a7d8f4a39aec7f02e72cb",
+  base: "https//api.openweathermap.org/data/2.5/"
+}
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
