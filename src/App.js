4b2085a1e0df4a0d3a1627970e217b805f2d3d78
mayuripandey <mayuripandey4@gmail.com>
2021-01-25 19:29:09 +0100

    weather app

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@
 import React from 'react';
 const api = {
   key: "2e9922a52a6a7d8f4a39aec7f02e72cb",
-  base: "https//api.openweathermap.org/data/2.5/"
+  base: "https://api.openweathermap.org/data/2.5/"
 }
 function App() {
   return (
-    <div className="App">
-      
+    <div className="app">
+      <main>
+        <div className="search-box">
+          <input
+           type="text"
+           className="search-box"
+           placeholder="Search...">
+           </input>
+        </div>
+      </main>
     </div>
   );
 }
