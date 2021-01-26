9cb01fed61bf742009f5dcb92371b1f842651d4c
mayuripandey <mayuripandey4@gmail.com>
2021-01-27 00:48:05 +0100

    weather app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 
-import React from 'react';
+import React, { useState } from 'react';
 const api = {
   key: "2e9922a52a6a7d8f4a39aec7f02e72cb",
   base: "https://api.openweathermap.org/data/2.5/"
 }
 function App() {
+ const [query, setQuery] = useState('');
+ const [weather, setWeather] = useState({}); 
 
-const dateBuilder = (d) => {
+ const search = evt => {
+   if (evt.key === "Enter") {
+     fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+     .then(res => res.json())
+     .then(result => {
+
+     setWeather(result);
+     setQuery('');
+     });
+
+   }
+ }
+
+ const dateBuilder = (d) => {
   let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ];
   let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
@@ -28,7 +43,10 @@ const dateBuilder = (d) => {
            type="text"
            className="search-box"
            placeholder="Search...">
-           </input>
+           onChange={e => setQuery(e.target)} 
+           value={query}
+           onKeyPress={search} 
+           ></input>
         </div>
         <div className="location-box">
           <div className="location">New York City, US</div>
