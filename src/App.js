6edac4042bd5a5dc863af9d99c539da6d620c7a4
mayuripandey <mayuripandey4@gmail.com>
2021-01-28 00:43:22 +0100

    weather app

--- src/App.js
+++ src/App.js
@@ -36,29 +36,29 @@ function App() {
 
 
   return (
-    <div className="app">
+    <div className={(typeof weather.main != "undefined") ? ((weather.main.temp > 16) ? 'app warm' : 'app') : 'app'}>
       <main>
         <div className="search-box">
           <input
            type="text"
            className="search-box"
            placeholder="Search..."
-           onChange={e => setQuery(e.target)} 
-           value={query}
+           onChange={e => setQuery(e.target.value)} 
+           value= {query}
            onKeyPress={search} 
            />
         </div>
         {(typeof weather.main != "undefined") ? (
         <div> 
         <div className="location-box">
-          <div className="location">(weather.name), {weather.sys.country}</div>
+          <div className="location">{weather.name}, {weather.sys.country}</div>
           <div className="date">{dateBuilder(new Date())}</div>
         </div>
         <div className="weather-box">
           <div className="temp">
-            15°C
+            {Math.round(weather.main.temp)}°c
           </div> 
-            <div className="weather">Sunny</div>
+            <div className="weather">{weather.weather[0].description}</div>
           </div>
         </div>
   ) : ('')}
